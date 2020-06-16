928cb99eb1b76bcc08fb0b5f1a692a5555a46339
Ryan McCue <me@ryanmccue.info>
2020-06-16 12:00:23 +0100

    Add missing Form index file
    importAll fails otherwise, annoyingly.

---
+++ src/renderer/components/Form/index.tsx
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as ButtonRow } from './ButtonRow';
+export { default as Checkbox } from './Checkbox';
+export { default as Row } from './Row';
+export { default as TextInput } from './TextInput';
+export { default as Toggle } from './Toggle';
